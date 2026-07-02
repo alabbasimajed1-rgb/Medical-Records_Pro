4f77e9c8226659d5401952ed8d5f5f6834471830
alabbasimajed1-rgb <alabbasimajed1@gmail.com>
2026-07-03 00:20:22 +0300

    Add Firestore import to home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:medical_app/services/firestore_service.dart';
 import 'package:pdf/pdf.dart';
