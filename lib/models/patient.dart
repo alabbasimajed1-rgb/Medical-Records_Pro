41d1fea06fc1ecba41da5ae6bc38d3138b532faf
alabbasimajed1-rgb <alabbasimajed1@gmail.com>
2026-07-02 18:19:48 +0300

    Add Patient model with toMap and fromMap methods

--- lib/models/patient.dart
+++ lib/models/patient.dart
@@ -1,1 +1,26 @@
+class Patient {
+  final String? id;
+  final String fullName;
+  final int age;
+  final String medicalHistory;
 
+  Patient({
+    this.id,
+    required this.fullName,
+    required this.age,
+    required this.medicalHistory,
+  });
+
+  Map<String, dynamic> toMap() => {
+        'fullName': fullName,
+        'age': age,
+        'medicalHistory': medicalHistory,
+      };
+
+  factory Patient.fromMap(String id, Map<String, dynamic> map) => Patient(
+        id: id,
+        fullName: map['fullName'] ?? '',
+        age: map['age'] ?? 0,
+        medicalHistory: map['medicalHistory'] ?? '',
+      );
+}
