c8980e098191256987b1126893a4cc5050e8d8c8
alabbasimajed1-rgb <alabbasimajed1@gmail.com>
2026-07-04 03:52:04 +0300

    Update patient.dart

--- lib/models/patient.dart
+++ lib/models/patient.dart
@@ -3,18 +3,21 @@ class Patient {
   final String fullName;
   final int age;
   final String medicalHistory;
+  final DateTime firstVisitDate; // هذه هي الكلمة التي كان يبحث عنها النظام!
 
   Patient({
     this.id,
     required this.fullName,
     required this.age,
     required this.medicalHistory,
+    required this.firstVisitDate,
   });
 
   Map<String, dynamic> toMap() => {
         'fullName': fullName,
         'age': age,
         'medicalHistory': medicalHistory,
+        'firstVisitDate': firstVisitDate.toIso8601String(), 
       };
 
   factory Patient.fromMap(String id, Map<String, dynamic> map) => Patient(
@@ -22,5 +25,8 @@ class Patient {
         fullName: map['fullName'] ?? '',
         age: map['age'] ?? 0,
         medicalHistory: map['medicalHistory'] ?? '',
+        firstVisitDate: map['firstVisitDate'] != null 
+            ? DateTime.parse(map['firstVisitDate']) 
+            : DateTime.now(), 
       );
 }
